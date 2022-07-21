a37716cc7da3c125d40b03c7048b310f11005e2f
Fengjun Yang <yangfengjun96@gmail.com>
2022-07-20 22:49:06 -0400

    implemented lspi

---
+++ tracking/lstd.py
@@ -0,0 +1,125 @@
+##############################################################################
+# Fengjun Yang, 2022
+# Least squares temporal difference learning for low-level tracking control.
+#
+# This is only applicable to value functions that are linear in features, and
+# is thus intended only to be used in testing.
+#
+# Much of this code has not been vectorized
+##############################################################################
+
+PI_TOL = 1e-3
+
+import numpy as np
+
+def _svec(X):
+    ''' Symmetric vectorization: convert a symmetric matrix X compactly into a
+    vector x in a way that
+                            <X, X> = <x, x>
+    Input:      - X:        np.array(N,N), Symmetric matrix X
+    Return:     - x:        np.array(N*(N+1)/2), X vectorized
+    '''
+    # Check that X is symmetric
+    assert len(X.shape) == 2 and X.shape[0] == X.shape[1]
+    assert np.allclose(X, X.T)
+
+    # Vectorize X
+    diagonal = np.diag(X)
+    triu = X[np.triu_indices_from(X, 1)]
+    x = np.concatenate([diagonal, np.sqrt(2) * triu])
+    return x
+
+
+def _smat(x):
+    ''' The inverse of _svec(). See _svec() above.
+    Input:      - x:        np.array(k), vector x
+    Return:     - X:        np.array(N,N), x converted into matrix
+    '''
+    # Check that dimension is valid
+    assert len(x.shape) == 1
+    N = int((np.sqrt(1+8*len(x))-1) / 2)
+    assert N*(N+1)/2 == len(x)
+
+    # Convert x into a matrix
+    diagonal = np.diag(x[:N])
+    triu = np.zeros((N, N))
+    triu[np.triu_indices_from(triu, 1)] = x[N:] / np.sqrt(2)
+    X = triu + triu.T + diagonal
+    return X
+
+def featurize(x, u, K, gamma):
+    ''' Featurize the state and control action.
+    Input:
+        - x:        np.array(p), states
+        - u:        np.array(q), control actions
+        - K:        np.array(q, p), static linear feedback controller
+        - gamma:    Float, discount factor
+    Output:
+        - phi:      np.array(f), the feature phi(x, u) as defined in the
+                    paper
+    '''
+    eta = gamma / (1-gamma)
+    xu = np.concatenate([x, u])
+    IK = np.vstack([np.eye(K.shape[1]), K])
+    return _svec(np.outer(xu, xu) + eta * IK @ IK.T)
+
+def lspi(traj, gamma, K0=None, max_iter=100):
+    ''' Least squares policy iteration for finding optimal LQR policy
+    Input:
+        - traj:         List of 4 tuples (x_k, u_k, r_k, x_{k+1})
+        - gamma:        discount factor
+        - K0:           initial policy
+        - max_iter:     maximum number of iterations
+    Output:
+        - K:            learned controller
+    '''
+    # Initialize controller
+    p = len(traj[0][0])
+    q = len(traj[0][1])
+    if K0 is None:
+        K = np.random.random((q, p))
+    else:
+        K = K0.copy()
+    # Policy iteration until convergence
+    for _ in range(max_iter):
+        P = evaluate(traj, K, gamma)
+        P12 = P[:p, p:]
+        P22 = P[p:, p:]
+        K_ = -np.linalg.pinv(P22) @ P12.T
+        if np.linalg.norm(K_ - K, 'fro') < PI_TOL:
+            break
+        K = K_
+    # Find the state value function
+    IK = np.vstack([np.eye(K.shape[1]), K])
+    P_state = IK.T @ P @ IK
+    return K, P_state
+
+def evaluate(traj, K, gamma):
+    ''' Evaluate a given controller K based on the collected data.
+    Input:
+        - traj:     List of 4 tuples (x_k, u_k, r_k, x_{k+1})
+        - K:        np.array(q, p), controller
+        - gamma:    Float, discount factor
+    Output:
+        - P:        State-action value matrix
+    '''
+    # Find the features of points on the trajectory
+    phi_xu = np.array([featurize(x, u, K, gamma) for (x, u, _, _) in traj])
+    phi_xx = np.array([featurize(x_, K @ x_, K, gamma) \
+                        for (_, _, _, x_) in traj])
+    # Evaluate the policy
+    A = np.einsum('ij,ik', phi_xu, (phi_xu-gamma*phi_xx))
+    b = np.sum( np.array([r for (_,_,r,_) in traj])[:,None] * phi_xu, 0)
+    p_hat = np.linalg.pinv(A) @ b
+    return _smat(p_hat)
+
+def construct_traj_list(xtrajs, utrajs, rtrajs):
+    ''' Convert a list of trajectories into the form that is taken by the
+    evaluate method.
+    '''
+    traj = []
+    for xtraj, utraj, rtraj in zip(xtrajs, utrajs, rtrajs):
+        T = utraj.shape[0]
+        traj += list(zip(xtraj[:T], utraj, rtraj, xtraj[1:]))
+    return traj
+
---
+++ tracking/test_utils.py
@@ -0,0 +1,85 @@
+##############################################################################
+# Fengjun Yang, 2022
+# Utility functions for testing the learned value and tracking controllers
+##############################################################################
+
+import numpy as np
+
+def random_env(p, q, Anorm=0.95):
+    #TODO: make sure that the system is controllable
+    ''' Generate a random linear dynamical system with specified dimensions.
+    Input:
+        - p:        Integer, state dimension
+        - q:        Integer, control dimension
+        - Anorm:    Float, spectral radius of A
+    Output:
+        - A:        np.array(p,p), dynamics matrix
+        - B:        np.array(p,q), controls matrix
+    '''
+    A = np.random.randn(p, p)
+    A = A / np.abs(np.linalg.eigvals(A)).max() * Anorm
+    B = np.random.randn(p, q)
+    return A, B
+
+def random_controller(q):
+    ''' Returns a random controller
+    Input:
+        - q:        Integer, control dimension
+    Return:
+        - ctrl:     function(p -> q), outputs a random control action
+    '''
+    return lambda x: np.random.random(q) - 0.5
+
+def zero_controller(q):
+    ''' Returns a zero controller
+    Input:
+        - q:        Integer, control dimension
+    Return:
+        - ctrl:     function(p -> q), outputs zero control
+    '''
+    return lambda x: np.zeros(q)
+
+def linear_feedback_controller(K):
+    ''' Returns a linear feedback controller
+    Input:
+        - K:        np.array, feedback gain
+    Return:
+        - ctrl:     function(p -> q), outputs zero control
+    '''
+    return lambda x: np.dot(K, x)
+
+def sample_traj(A, B, Q, R, ctrl, T, x0=None):
+    ''' Given an environment, sample a trajectory of T time steps
+    Input:
+        - A, B:     dynamics of the system
+        - K:        controller
+        - Q, R:     cost structure
+        - T:        length of trajectory
+        - x0:       initial state. If None, it will be sampled at random
+    Return:
+        - xtraj:    np.array(T+1, p), trajectory of state
+        - utraj:    np.array(T, q), trajectory of control
+        - rtraj:    np.array(T), cost at each step
+    '''
+    # Generate x0 if x0 is None
+    if x0 is None:
+        x0 = np.random.randn(A.shape[0])
+
+    # Initialize variables
+    p, q = B.shape
+    xtraj = np.zeros((T+1, p))
+    utraj = np.zeros((T, q))
+    rtraj = np.zeros(T)
+
+    # Simulate forward
+    x = x0.copy()
+    xtraj[0] = x
+    for t in range(T):
+        u = ctrl(x)
+        x_ = A @ x + B @ u
+        r = np.dot(Q @ x, x) + np.dot(R @ u, u)
+        utraj[t] = u
+        rtraj[t] = r
+        xtraj[t+1] = x_
+        x = x_
+    return xtraj, utraj, rtraj
